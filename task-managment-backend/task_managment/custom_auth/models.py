4d53a29abece02c6d293c1bca71c06bfa98a5b2b
Leo <borisevichlevik@gmail.com>
2025-01-27 18:16:53 +0300

    message

---
+++ task-managment-backend/task_managment/custom_auth/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, AbstractUser
+from django.db import models
+
+from django.utils.translation import gettext_lazy as _
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
